173217edc0eddca5c54cbd6a10c3fd7393199844
vincennlin <149757061+vincennlin@users.noreply.github.com>
2026-07-06 14:20:19 +0800

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        backtrack(nums, res, 0);
+        return res;
+    }
+
+    void backtrack(vector<int>& nums, vector<vector<int>>& res, int i) {
+        if (i == nums.size()) {
+            res.push_back(nums);
+            return;
+        }
+
+        for (int j = i; j < nums.size(); j++) {
+            swap(nums[i], nums[j]);
+            backtrack(nums, res, i + 1);
+            swap(nums[i], nums[j]);
+        }
+    }
+};
