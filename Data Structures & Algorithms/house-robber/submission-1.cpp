57f3ba424011fd1cfb286b0532107d60c4d3f7b1
vincennlin <149757061+vincennlin@users.noreply.github.com>
2026-07-10 11:51:33 +0800

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int rob1 = 0, rob2 = 0;
+
+        for (int num : nums) {
+            int temp = max(num + rob1, rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
+
+        return rob2;
+    }
+};
